9c4ea387cd9b3285ab09623982402d7d40b5a86a
Shah Rukh Qasim <14beesqasim@seecs.edu.pk>
2015-11-01 21:17:17 +0500

    Added command and install scripts

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+FILE="/tmp/out.$$"
+GREP="/bin/grep"
+#....
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+cp q /bin/
+chmod a+x /bin/q
---
+++ q
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+eval $@ "> /tmp/t19090.tmp 2> /tmp/t19091.tmp &"
+
